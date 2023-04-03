4416d4c1108f89db972161d43dbb86e3d30cd87e
Distiller <distiller@static.38.39.183.45.cyberlynk.net>
2023-04-03 08:11:22 +0000

    Update Package.swift to 2.0.0

--- Package.swift
+++ Package.swift
@@ -1,21 +1,35 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "TwilioSyncClient",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "TwilioSyncClient",
-            targets: ["TwilioSyncClient"]),
+            targets: ["TwilioSyncClientTarget"]),
+    ],
+    dependencies: [
+        .package(
+            name: "TwilioTwilsockLib",
+            url: "https://github.com/twilio/twilsock-ios",
+            .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
+        .target(
+          name: "TwilioSyncClientTarget",
+          dependencies: [
+              .target(name: "TwilioSyncClient"), 
+              .product(name: "TwilioTwilsockLib", package: "TwilioTwilsockLib")
+          ],
+          path: "Dummy"
+        ),
         .binaryTarget(
             name: "TwilioSyncClient",
-            url: "https://sdk.twilio.com/ios/sync/releases/1.2.1/twilio-sync-ios-1.2.1.zip",
-            checksum: "2dd01bea43263c616c96ec1caf439f1b0a9e9b5aced3da140c102a112e0d4278"
-        )
+            url: "https://sdk.twilio.com/ios/sync/releases/2.0.0/twilio-sync-ios-2.0.0.zip",
+            checksum: "27dbf067f48628bb50f1289ea50e1ac08142fdb751df0544d4de91da1fbd6294"
+        ),
     ]
 )
